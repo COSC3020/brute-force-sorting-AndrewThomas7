e442b3cb8fc911196555b45031e1737cdfd25fb3
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-04-09 21:43:02 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,21 @@
+function Swap(array, first, second) {
+    var temp = array[first];
+    array[first] = array[second];
+    array[second] = temp;
+    return array;
+}
 
-function ElementCompare2d(Twodarr,element2){
-    var equal=false;
-    for(x=0;x<Twodarr.length;x++){
-        var count=0;
-        for(y=0;y<element2.length;y++){
-            if(Twodarr[x][y]==element2[y]){
+
+function ElementCompare2d(Twodarr, element2) {
+    var equal = false;
+    for (x = 0; x < Twodarr.length; x++) {
+        var count = 0;
+        for (y = 0; y < element2.length; y++) {
+            if (Twodarr[x][y] == element2[y]) {
                 count++;
             }
-            if(count>=element2.length){
-                equal=true;
+            if (count >= element2.length) {
+                equal = true;
             }
 
         }
@@ -16,25 +23,13 @@ function ElementCompare2d(Twodarr,element2){
     return equal
 }
 
-function IsElement(array,element){
-    var ElementExists=false;
-    for(var x=0;x<array.length;x++){
-        if(array[x]==element){
-            ElementExists=true
-        }
+function Factorial(n) {
+    if (n == 0) {
+        return 1;
     }
-    return ElementExists
+    return Factorial(n - 1) * n
 }
 
-function Max(array){
-    var max;
-    for(var y=0; y<array.length;y++){
-        if(array[y]<=array[y+1]){
-            max=array[y+1]
-        }
-    }
-    return max;
-}
 function Sorted(array){
     sorted=false;
     for(var x=0;x<array.length-1;){
@@ -50,31 +45,28 @@ function Sorted(array){
     return sorted
 }
 
-function Factorial(n){
-    if(n==0){
-        return 1;
-    }
-    return Factorial(n-1)*n
-}
-function perm(array){
-    var permlist=[];
-    while(permlist.length!=Factorial(array.length)){
-        var perm=[];
-        while(perm.length!=array.length){
-            var generate=Math.floor((Math.random()*Math.max(...array)+1))
-            if(IsElement(array,generate)&& IsElement(perm,generate)==false){
-                perm.push(generate)
+function Permutation(array) {
+    var perm;
+    var permlist = []
+    function Permutation_(array, FixIndex, count, iter) {
+        if (FixIndex >= array.length - 1) {
+            return permlist
+        }
+        for (var i = iter; i < array.length; i++) {
+            Swap(array, FixIndex, i)
+            if(ElementCompare2d(permlist,array.slice())==false){
+                permlist.push(array.slice())
             }
+            Permutation_(array, FixIndex + 1, count, iter + 1)
+            Swap(array, FixIndex, i)
         }
-        if(ElementCompare2d(permlist,perm)==false){
-            permlist.push(perm)
-        }
-    }   
-    return permlist  
+        return permlist
+    }
+    return Permutation_(array,0,0,0)
 }
 
 function permutationSort(array){
-    var permlist=perm(array);
+    var permlist=Permutation(array);
     var returnarr;
     var count=0;
     for(x=0;x<permlist.length;x++){
